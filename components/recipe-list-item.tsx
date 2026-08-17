fc2307d087cfa10e125c626905f2610c18581ce3
Rico Hancock <ricohancock@proton.me>
2026-08-17 10:39:49 -0700

    refactor: :sparkles: Simplify RecipeListItem component by consolidating props destructuring for improved readability

--- components/recipe-list-item.tsx
+++ components/recipe-list-item.tsx
@@ -2,14 +2,10 @@
 
 import { Checkbox } from "./ui/checkbox";
 
-export function RecipeListItem({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
+export function RecipeListItem({ children }: { children: React.ReactNode }) {
 	return (
 		<li className="relative">
-			<label className="[*:has([data-state='checked'])]:line-through [*:has([data-state='checked'])]:opacity-70">
+			<label className="[*:has([data-state='checked'])]:line-through [*:has([data-state='checked'])]:opacity-70 [&_a]:underline [&_a]:text-violet-400">
 				<Checkbox className="absolute right-full top-1 mr-2" />
 				{children}
 			</label>
